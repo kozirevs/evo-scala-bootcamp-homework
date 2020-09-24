7dd96612ad992e939070fa908f574eb611758f6d
akoz <kozirevs@inbox.lv>
2020-09-24 10:48:43 +0300

    Add tests.

--- src/main/scala/basics/Basics.scala
+++ src/main/scala/basics/Basics.scala
@@ -1,26 +1,11 @@
 package basics
 
+import scala.annotation.tailrec
+
 object Basics {
 
-  def main(args: Array[String]): Unit = {
-    println("lcm = " + lcm(35, 15))
+  def lcm(a: Int, b: Int): Int = if ((a == 0) || (b == 0)) 0 else Math.abs(a * b) / gcd(a, b)
 
-    println("gcd = " + gcd(70, 30))
-  }
-
-  def lcm(a: Int, b: Int): Int = {
-    if ((a == 0) || (b == 0)) 0
-    else {
-      val absA = Math.abs(a)
-      val absB = Math.abs(b)
-      val absHigherNumber = Math.max(absA, absB)
-      val absLowerNumber = Math.min(absA, absB)
-      var lcm = absHigherNumber
-      while (lcm % absLowerNumber != 0)
-        lcm += absHigherNumber
-      lcm
-    }
-  }
-
+  @tailrec
   def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
 }
---
+++ src/test/scala/basics/BasicsSpec.scala
@@ -0,0 +1,27 @@
+package basics
+
+import basics.Basics.{gcd, lcm}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+
+
+class BasicsSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks {
+
+  "lcm method" should "return lowest common multiple for two integers" in {
+    lcm(35, 15) shouldEqual 105
+    lcm(3, 7) shouldEqual 21
+    lcm(25, 75) shouldEqual 75
+    lcm(18, 15) shouldEqual 90
+    lcm(21, 15) shouldEqual 105
+  }
+
+  "gcd method" should "return greatest common divisor for two integers" in {
+    gcd(70, 30) shouldEqual 10
+    gcd(12, 36) shouldEqual 12
+    gcd(7, 9) shouldEqual 1
+    gcd(28, 64) shouldEqual 4
+    gcd(48, 36) shouldEqual 12
+  }
+
+}
